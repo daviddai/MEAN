8bc205afc869f5938cc31bc3b719b7658b3ee341
daviddai <david.dai.personal@gmail.com>
2018-03-03 11:29:13 +1100

    implemented router guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { routing } from './app.routes';
 import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { UserLoginComponent } from './components/user-login/user-login.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
-import {ToggleService} from './shared/toggle-service.service';
+import { ToggleService } from './shared/toggle-service.service';
+
+import { routing } from './app.routes';
+
+import { JwtInterceptor } from './interceptors/jwt-interceptor';
+import {CanActivateUser} from './router-guards/can-activate-user';
 
 
 @NgModule({
@@ -24,9 +29,18 @@ import {ToggleService} from './shared/toggle-service.service';
     BrowserModule,
     routing,
     NgbCollapseModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
-  providers: [ToggleService],
+  providers: [
+    ToggleService,
+    CanActivateUser,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,16 @@ import {RouterModule, Routes} from '@angular/router';
 import {UserLoginComponent} from './components/user-login/user-login.component';
 import {ModuleWithProviders} from '@angular/core';
 import {HomeComponent} from './components/home/home.component';
+import {CanActivateUser} from './router-guards/can-activate-user';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'login', component: UserLoginComponent }
+  { path: '',
+    component: HomeComponent,
+    canActivate: [ CanActivateUser ]
+  },
+  { path: 'login',
+    component: UserLoginComponent
+  }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
