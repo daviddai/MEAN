d4049a7ecf9c1fcfbfb5af97ee318041198f2afb
daviddai <david.dai.personal@gmail.com>
2018-03-11 21:15:34 +1100

    implemented matching password validation

--- src/app/components/user-register/user-register.component.ts
+++ src/app/components/user-register/user-register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { RegisterUser } from '../../models/register-user';
+import { confirmPasswordValidator, emailFormatValidator } from '../../utils/validators/validators';
 
 @Component({
   selector: 'app-user-register',
@@ -8,12 +11,58 @@ import { Router } from '@angular/router';
 })
 export class UserRegisterComponent implements OnInit {
 
-  constructor(private router: Router) {}
+  public registrationForm: FormGroup;
+  public registerUser: RegisterUser;
 
-  ngOnInit() {}
+  constructor(private router: Router) {
+    this.registerUser = new RegisterUser();
+  }
+
+  ngOnInit() {
+    this.registrationForm = new FormGroup({
+      'name': new FormControl(this.registerUser.name, [
+        Validators.required
+      ]),
+      'email': new FormControl(this.registerUser.email, [
+        Validators.required,
+        emailFormatValidator()
+      ]),
+      'password': new FormControl(this.registerUser.password, [
+        Validators.required
+      ]),
+      'confirmPassword': new FormControl(this.registerUser.confirmPassword, [
+        Validators.required,
+        confirmPasswordValidator(() => {
+          return this.registerUser.password;
+        })
+      ]),
+    });
+  }
+
+  register() {}
 
   goToLogin() {
     this.router.navigate(['/login']);
+  }
+
+  get name() {
+    return this.registrationForm.get('name');
+  }
+
+  get email() {
+    return this.registrationForm.get('email');
+  }
+
+  get password() {
+    return this.registrationForm.get('password');
+  }
+
+  get confirmPassword() {
+    return this.registrationForm.get('confirmPassword');
+  }
+
+  isInputInvalid(formControlName) {
+    return this.registrationForm.get(formControlName).invalid;
   }
 
 }
